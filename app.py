c4ff8217711fe2c7602fa12139c0d3785db5e053
Artem Shelukhin <Artem_Shelukhin@epam.com>
2026-01-25 22:03:44 +0000

    intentional bug

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def health():
 
 if __name__ == '__main__':
     import os
-    app.run(host='0.0.0.0', port=5000, debug=os.environ.get('FLASK_DEBUG', 'False') == 'True')
+    app.run(host='0.0.0.0', port=1000, debug=os.environ.get('FLASK_DEBUG', 'False') == 'True')
