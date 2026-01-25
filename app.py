d949b9e4a0e8f7ff158411c5dda6488abba3086a
Artem Shelukhin <Artem_Shelukhin@epam.com>
2026-01-25 23:03:19 +0000

    intentional bug

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def index():
     return 'test1235'
 
 
-@app.route('/health')
-def health():
-    return 'ok', 200
+# @app.route('/health')
+# def health():
+#     return 'ok', 200
 
 
 if __name__ == '__main__':
     import os
-    app.run(host='0.0.0.0', port=1000, debug=os.environ.get('FLASK_DEBUG', 'False') == 'True')
+    app.run(host='0.0.0.0', port=8000, debug=os.environ.get('FLASK_DEBUG', 'False') == 'True')
